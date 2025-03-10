7f21649843dd371548a11258c0230b4f29431824
EnricoCiccolini <piroroma90@gmail.com>
2025-03-10 15:02:32 +0100

    feat: add ja dom schedule

--- script.js
+++ script.js
@@ -36,3 +36,37 @@ const teamMembers = [
     img: "img/female3.png"
   }
 ];
+
+console.log('ciao')
+const tableElement = document.getElementById('table')
+let result = ''
+
+for (let i = 0; i < teamMembers.length; i++) {
+  const { name, role, email, img } = teamMembers[i]
+  console.log(name)
+  console.log(role)
+  console.log(email)
+  console.log(img)
+
+  result += `
+                <div class=" col-4 mb-3" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src=${img} class="img-fluid " alt="male1">
+                        </div>
+                        <div class="col-md-8 bg-black">
+                            <div class="card-body text-white ">
+                                <h5 class="card-title">${name}</h5>
+                                <p class="card-text">${role}</p>
+                                <p class="card-text text-info">${email}</small></p>
+                            </div>
+                        </div>
+                      </div>
+                  </div>`
+
+
+
+
+
+}
+tableElement.innerHTML = result
